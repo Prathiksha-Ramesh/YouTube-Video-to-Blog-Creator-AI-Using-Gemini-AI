913c5af6050356433fdd0ad57a80d38660800daf
Prathiksha Ramesh <prathiksha_2001@outlook.com>
2024-08-29 17:01:05 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -3,5 +3,5 @@ from tools import tool
 
 #Initialize the tool with the specific youtube channel handle to target your search:
 
-yt_tool=YoutubeChannelSearchTool(youtube_channel_handle='@krishnaik06')
+yt_tool=YoutubeChannelSearchTool(youtube_channel_handle='')
 
